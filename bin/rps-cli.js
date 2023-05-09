e73073e140ca9495c8594d0359d28154acc2ef2b
Tyrus <tyrus@email.unc.edu>
2023-05-09 06:51:32 -0400

    added rules

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -11,4 +11,8 @@ if (args.h || args.help) {
     console.log(' Usage: node-rps [SHOT]', '\n', 'Play Rock Paper Scissors (RPS)', '\n\n', '  -h, --help      display this help message and exit', '\n', '  -r, --rules     display the rules and exit', '\n', 'Examples:', '\n', '  node-rps        Return JSON with single player RPS result.', '\n', '                  e.g. {"player":"rock"}', '\n', '  node-rps rock   Return JSON with results for RPS played against a simulated opponent.', '\n', '                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
 }
 
+if (args.r || args.rules) {
+    console.log('Rules for Rock Paper Scissors:', '\n\n', '    - Scissors CUTS Paper', '\n', '    - Paper COVERS Rock', '\n', '    - Rock CRUSHES Scissors');
+}
+
 console.log(JSON.stringify(rps(args._[0])));
