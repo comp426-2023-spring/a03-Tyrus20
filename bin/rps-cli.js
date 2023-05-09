4f79e9c78fa4368f706cc48788e6d118d7d744f1
Tyrus <tyrus@email.unc.edu>
2023-05-09 05:02:29 -0400

    imported more things

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -3,3 +3,4 @@
 //command line interface for rock-paper-scissors
 
 import minimist from 'minimist';
+import { rps } from '../lib/rps.js';
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
 //command line interface for rock-paper-scissors-lizard-spock
+
+import minimist from 'minimist';
+import { rpsls } from '../lib/rpsls.js';
